cd9fe3e23e3724f308ccf6ca2fb7ef885216b9fc
Rahul Pathak <156890065+RahulPathak17072005@users.noreply.github.com>
2026-04-05 17:01:28 +0530

    Add DiscountAmount class to calculate discount and final fee

---
+++ core-programming-basics/Programming-Elements/level1/DiscountAmount.java
@@ -0,0 +1,13 @@
+public class DiscountAmount {
+    public static void main(String[] args){
+    int fee= 125000;
+    int discountPercentage = 10;
+
+    double discount= (fee * discountPercentage) / 100.0;
+    double finalFee =  fee - discount;
+    System.out.println(
+        " The discount amount is INR"+ discount + " and final discounted fee is INR " +finalFee
+    );
+}
+
+}
