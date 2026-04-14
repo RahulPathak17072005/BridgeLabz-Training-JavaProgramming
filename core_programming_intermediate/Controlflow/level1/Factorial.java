a78d8494a50366ce56baa0c7550e23d94969e934
Rahul Pathak <156890065+RahulPathak17072005@users.noreply.github.com>
2026-04-14 14:10:23 +0530

    Implement factorial calculation using a while loop

--- core_programming_intermediate/Controlflow/level1/Factorial.java
+++ core_programming_intermediate/Controlflow/level1/Factorial.java
@@ -3,6 +3,17 @@ import java.util.*;
 
 class Factorial {
 public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter the number: ");
+    int num=sc.nextInt();
+    int i=1;
+    int ans=1;
+    while(i<=num){
+        ans *=i;
+        i++;
+    }
+    System.out.println(ans);
+    sc.close();
     
 }
     
