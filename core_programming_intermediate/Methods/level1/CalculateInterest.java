0c305d59c37993ddcc44da7663a0ecd4e6f95606
Rahul Pathak <156890065+RahulPathak17072005@users.noreply.github.com>
2026-04-15 16:36:25 +0530

    Add CalculateInterest class to compute simple interest based on user input

---
+++ core_programming_intermediate/Methods/level1/CalculateInterest.java
@@ -0,0 +1,23 @@
+package core_programming_intermediate.Methods.level1;
+import java.nio.channels.Pipe.SourceChannel;
+import java.util.*;
+
+public class CalculateInterest {
+    static int SimpleInterest(int principle,int rate,int time){
+        int si=(principle*rate*time)/100;
+        return si;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter principle: ");
+        int p=sc.nextInt();
+        System.out.print("Enter the rate: ");
+        int r=sc.nextInt();
+        System.out.print("Enter the time: ");
+        int t=sc.nextInt();
+        System.out.println(SimpleInterest( p, r, t));
+        sc.close();
+    }
+
+    
+}
