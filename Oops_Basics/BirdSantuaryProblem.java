13eb02eb9affc59d39c42824e0422b43b7288b7e
Rahul Pathak <156890065+RahulPathak17072005@users.noreply.github.com>
2026-04-27 15:23:00 +0530

    Add BirdSantuaryProblem class and update .gitignore to include .class files

---
+++ Oops_Basics/BirdSantuaryProblem.java
@@ -0,0 +1,16 @@
+package Oops_Basics;
+import java.util.*;
+class Flyable{
+String Bird;
+
+}
+class Swimmable{
+
+}
+
+public class BirdSantuaryProblem {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        sc.close();
+    }
+}
