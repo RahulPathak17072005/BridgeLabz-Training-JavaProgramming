1a8bcd5733a16af928993ca7dd63d7d01dfe19b9
Rahul Pathak <156890065+RahulPathak17072005@users.noreply.github.com>
2026-05-07 12:12:49 +0530

    Add practice questions

---
+++ Oops_Basics/Student.java
@@ -0,0 +1,15 @@
+/*package Oops_Basics;
+
+import java.util.*;
+public class Student{
+    public static void main(String[] args){
+        String s="Mr. Rahul Pathak";
+        String[] words=s.split(" ");
+        StringBuilder result=new StringBuilder();
+        for(String word:words){
+            int length=word.length();
+        }
+        
+
+    }
+}*/
