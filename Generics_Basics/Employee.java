b81d48a0e741dc6113ae22103c97eb6e58ce5316
Rahul Pathak <156890065+RahulPathak17072005@users.noreply.github.com>
2026-05-26 11:31:51 +0530

    Add Employee Record java program

---
+++ Generics_Basics/Employee.java
@@ -0,0 +1,46 @@
+package Generics_Basics;
+import java.util.HashMap;
+import java.util.Scanner;
+public class Employee {
+int id;
+    String name;
+    String department;
+
+    Employee(int id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    void display() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Department: " + department);
+    }
+}
+
+ class Main {
+    public static void main(String[] args) {
+
+        HashMap<Integer, Employee> map = new HashMap<>();
+
+        map.put(101, new Employee(101, "Rahul", "IT"));
+        map.put(102, new Employee(102, "Pathak", "HR"));
+        map.put(103, new Employee(103, "Kunal", "Finance"));
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Employee ID to search: ");
+        int searchId = sc.nextInt();
+
+        if (map.containsKey(searchId)) {
+            System.out.println("Employee Found:");
+            map.get(searchId).display();
+        } else {
+            System.out.println("Employee not found");
+        }
+
+        sc.close();
+    }
+    
+}
